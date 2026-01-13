7cfe2262b6ccb1281566b5863d395b7616be2e3e
fedder <jed.lordy123@gmail.com>
2026-01-13 20:10:06 +0800

    4

--- api/projects.js
+++ api/projects.js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
     const projects = await Project.find({});
     res.status(200).json(projects);
   } catch (error) {
+    console.log("proj err", error);
     res.status(500).json({ error: 'Failed to fetch projects' });
   }
 }
