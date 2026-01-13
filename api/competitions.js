c2febe4753b2e10bd56e357cb40bda16f772d72e
fedder <jed.lordy123@gmail.com>
2026-01-13 20:09:30 +0800

    3

--- api/competitions.js
+++ api/competitions.js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
     const competitions = await Competition.find({});
     res.status(200).json(competitions);
   } catch (error) {
+    console.log("Compe er", error);
     res.status(500).json({ error: 'Failed to fetch competitions' });
   }
 }
